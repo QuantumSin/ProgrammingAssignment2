bea0100e1f5b5a2d4c56e24de1c5a02b73f7d5a1
QuantumSin <bbucklein@gmail.com>
2023-07-10 13:21:46 -0500

    Working system

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions use lexical scoping to find the inverse of a matrix, but if
+## the inverse has already been calculated, it will return a cached copy instead.
 
-## Write a short comment describing this function
+## Creates an object of class makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Checks for an existing inverse, returning cached value or calculating and
+## caching it for later retrieval
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinv(inv)
+    inv
 }
